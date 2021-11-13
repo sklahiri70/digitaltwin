449d9812edf03e176c596ea1457457c2b49e1ce1
sklahiri70 <94220005+sklahiri70@users.noreply.github.com>
2021-11-13 16:07:30 +0530

    Update demo6.py

--- demo6.py
+++ demo6.py
@@ -8,11 +8,7 @@ import streamlit as st
 import plotly.express as px
 #######################################
 
-from streamlit_autorefresh import st_autorefresh
 
-# Run the autorefresh about every 2000 milliseconds (2 seconds) and stop
-# after it's been refreshed 100 times.
-count = st_autorefresh(interval=2000, limit=10, key="fizzbuzzcounter")
 
 
 ####################################
